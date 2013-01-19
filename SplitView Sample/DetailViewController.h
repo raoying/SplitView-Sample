8dff8f0ee42162b61ad34bc4837bf19bfaf50612
Ying Rao <raoying@gmail.com>
2013-01-19 01:17:41 -0600

    Removed the UISplitViewControllerDelegate protocol from DeailViewController.

--- SplitView Sample/DetailViewController.h
+++ SplitView Sample/DetailViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+@interface DetailViewController : UIViewController
 
 @property (strong, nonatomic) id detailItem;
 
